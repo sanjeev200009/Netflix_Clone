68284b14ab1e0bee3747821ba4b2aa57717cbf3d
sanjeev200009 <sanjaysanjeev2000@gmail.com>
2025-05-22 19:32:58 +0530

    change for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Netflix_Clone/' : '/', //
   server: {
     host: "::",
     port: 8080,
